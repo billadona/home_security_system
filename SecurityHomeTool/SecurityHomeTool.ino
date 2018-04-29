bf64d565c1c4dc2cca70d6f2ca0d78517c48df97
bill.adona@ucdenver.edu <bill.adona@ucdenver.edu>
2018-04-28 23:34:02 -0600

    Bug fixed, working on algorithm to recognize knock

--- SecurityHomeTool/SecurityHomeTool.ino
+++ SecurityHomeTool/SecurityHomeTool.ino
@@ -7,9 +7,9 @@
   port, and toggles the LED on pin 13.
 
   The circuit:
-	- positive connection of the piezo attached to analog in 0
-	- negative connection of the piezo attached to ground
-	- 1 megohm resistor attached from analog in 0 to ground
+  - positive connection of the piezo attached to analog in 0
+  - negative connection of the piezo attached to ground
+  - 1 megohm resistor attached from analog in 0 to ground
 
   created 25 Mar 2007
   by David Cuartielles <http://www.0j0.org>
@@ -37,6 +37,7 @@ unsigned long currentTime = 0.0;
 unsigned long knockTimes[10];
 unsigned long knockAttempt[10];
 unsigned int i = 0;
+unsigned int j = 0;
 bool secondKnock = true;
 
 void setup() {
@@ -49,53 +50,49 @@ void loop() {
   if(i == 0)
   {
     // record initial knock password
-    recordPattern(knockTimes);
+    recordPattern(knockTimes, i);
+    Serial.println("Knock pattern stored");
+    for(int ind = 0; ind < i; ind++)
+    {
+      Serial.println(knockTimes[ind]);
+    }    
+    Serial.print("this is the last input ");
+    Serial.println(knockTimes[i-1]);
   }
   else
   {
     // listen for knock pattern to unlock
-    while(!determinePattern())
-    {
-      
-    }
+    recordPattern(knockAttempt, j);
   }
-  delay(100);  // delay to avoid overloading the serial port buffer
 }
 
-void recordPattern(unsigned long* arr) 
+void recordPattern(unsigned long* arr, unsigned int& i) 
 {
   while(true)
   {
     sensorReading = analogRead(knockSensor);
     if((millis() - startTime) > 5000)
     {
-      Serial.println("Knock pattern stored");
-      // listen for the knock pattern
-      for(int ind = 0; ind < i; ++ind)
-      {
-        Serial.println(knockTimes[ind]);
-        return;
-      }
+      break;
     }
-  
     // if the sensor reading is greater than the threshold:
     if (sensorReading >= threshold) 
     {
       if(secondKnock == true && i == 0)                  // listen for very first knock
       {
         startTime = millis();                            // start timer
-        Serial.print("SecondKnock true ");
-        Serial.println(secondKnock);
+        Serial.println(i);
         secondKnock = false;                             // set the boolean
+        Serial.println(analogRead(knockSensor));         
       }
       else if(secondKnock == true && i > 0)
       {
         currentTime = millis();
         arr[i] = currentTime - startTime;
         ++i;
-        Serial.print("SecondKnock true ");
-        Serial.println(secondKnock);      
+        Serial.println(i);    
         secondKnock = false;
+        Serial.println(analogRead(knockSensor));
       }
       else                                               // listen for second knock
       {
@@ -104,23 +101,16 @@ void recordPattern(unsigned long* arr)
         ++i;
         startTime, currentTime = 0.0;
         startTime = millis();                            // restart the time for next knock
-        Serial.print("SecondKnock false ");
-        Serial.println(secondKnock);
+        Serial.println(i);        
         secondKnock = true;
+        Serial.println(analogRead(knockSensor));
       }
-      // toggle the status of the ledPin:
-      ledState = !ledState;
-      // update the LED pin itself:
-      digitalWrite(ledPin, ledState);
-      // send the string "Knock!" back to the computer, followed by newline
-      Serial.println(analogRead(knockSensor));
-    }    
+    } 
+    sensorReading = 0; 
+    delay(112);  
   }
+  sensorReading = 0;
+  startTime, currentTime = 0.0;
 }
 
-boolean determinePattern()
-{
-  recordPattern(knockAttempt);
-  return true;
-}
 
